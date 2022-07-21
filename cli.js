3ad3e68f7871dd7e902ef69f0f109d141e2076e2
Frieder Bluemle <frieder.bluemle@gmail.com>
2022-07-21 13:50:10 -0700

    Fix formatting

--- cli.js
+++ cli.js
@@ -3,24 +3,24 @@ import meow from 'meow';
 import unicornFun from 'unicorn-fun';
 
 const cli = meow(`
-	Usage
-	  $ unicorn-fun [input]
+  Usage
+    $ unicorn-fun [input]
 
-	Options
-	  --postfix  Lorem ipsum  [Default: rainbows]
+  Options
+    --postfix  Lorem ipsum  [Default: rainbows]
 
-	Examples
-	  $ cli-name
-	  unicorns & rainbows
-	  $ cli-name ponies
-	  ponies & rainbows
+  Examples
+    $ cli-name
+    unicorns & rainbows
+    $ cli-name ponies
+    ponies & rainbows
 `, {
-	flags: {
-		postfix: {
-			type: 'string',
-			default: 'rainbows'
-		}
-	}
+  flags: {
+    postfix: {
+      type: 'string',
+      default: 'rainbows'
+    }
+  }
 });
 
 console.log(moduleName(cli.input[0] || 'unicorns', cli.flags));
--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import test from 'ava';
 import execa from 'execa';
 
 test('main', async t => {
-	const {stdout} = await execa('./cli.js', ['ponies']);
-	t.is(stdout, 'ponies & rainbows');
+  const {stdout} = await execa('./cli.js', ['ponies']);
+  t.is(stdout, 'ponies & rainbows');
 });
